10485fbbb11ede9b8801a506cd992ec63a0c5199
Samira M. Pantaleão <168239105+samirampanta@users.noreply.github.com>
2024-08-09 22:29:11 -0300

    Add files via upload

---
+++ exercicio3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	float n1, n2, n3, n4, resul;
+	char escolha;
+	printf("Escolha o tipo de media:\nA - Aritimetica\nP - Ponderada\nH - Harmonica\n");
+	scanf("%c",&escolha);
+	printf("Nota 1: ");
+	scanf("%f",&n1);
+	printf("Nota 2: ");
+	scanf("%f",&n2);
+	printf("Nota 3: ");
+	scanf("%f",&n3);
+	printf("Nota 4: ");
+	scanf("%f",&n4);
+	if(escolha == 'A'){
+		
+	}
+	return 0;
+}
